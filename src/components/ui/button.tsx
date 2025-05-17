7bf0ce61110f37e85a7b4db508e1896c03a32a39
urpapi <111202315074@mhs.dinus.ac.id>
2025-05-18 00:43:23 +0700

    v6

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,10 +3,7 @@ import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
-const socialClass =
-    "text-animation flex items-center gap-1 px-0 hover:px-1 rounded-sm border-1 border-background " +
-    "hover:border-b-1 hover:border-black hover:border-dashed hover:bg-amber-300 dark:hover:bg-green-800 dark:hover:border-amber-50 " +
-    "active:border-b-1 active:border-black active:border-dashed active:bg-amber-300 dark:active:bg-green-800 dark:active:border-amber-50";
+
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
