f23b4219920a08b9cfb6828e7ae0f63643c9ef71
urpapi <111202315074@mhs.dinus.ac.id>
2025-05-19 12:56:39 +0700

    v8

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -5,47 +5,35 @@ import type { Metadata } from 'next';
 import ArticleMain  from '@/app/components/article-main';
 
 
-type Props = {
-  params: { slug: string };
-};
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const post = allPosts.find(p => p._raw.flattenedPath === `posts/${params.slug}`);
+type Params = Promise<{ slug: string }>
+ 
+export async function generateMetadata({ params }: { params: Params }) {
+  const { slug } = await params;
+  const post = allPosts.find(p => p._raw.flattenedPath === slug);
 
   if (!post) {
-    return { title: 'Post Not Found | Sonarkode' };
+    return {
+      title: 'Post Not Found',
+    };
   }
 
   return {
-    title: `${post.title} | Sonarkode`,
+    title: post.title,
     description: post.summary,
-    authors: post.author ? [{ name: post.author }] : undefined,
-    keywords: post.category,
-    openGraph: {
-      title: post.title,
-      url: `https://sonarkode.blog/posts/${params.slug}`,
-      type: 'article',
-      authors: post.author ? [post.author] : undefined,
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title: `${post.title} | Sonarkode`,
-      creator: '@ferdianfarizaa',
-    },
   };
+
 }
 
-export async function generateStaticParams(): Promise<Props['params'][]> {
-  return allPosts.map((post) => ({
-    slug: post._raw.flattenedPath.replace('posts/', ''),
-  }));
-}
-
-export default function PostPage({ params }: Props) {
-  const post = allPosts.find(p => p._raw.flattenedPath === `posts/${params.slug}`);
-  if (!post || !post.body.code) {
+export default async function PostPage({
+    params,
+}: {
+    params: Params;
+}) {
+    const  {slug}  = await params;
+    const post = allPosts.find(p => p._raw.flattenedPath === slug);
+    
+    if (!post) {
     notFound();
   }
-
-  return <ArticleMain post={post} />;
+    return <ArticleMain post={post} />;
 }
